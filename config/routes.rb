8438aaaafd69228eb9df65fcf3c31d963434132a
Mark Otuya <markotuya0@gmail.com>
2022-12-09 17:04:10 +0100

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :categories, except: [:update, :edit]
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :categories, except: [:update, :edit, :destroy] do
+    resources :expenses, only: [:new, :create]
+  end
   root  "home#index"
 end
